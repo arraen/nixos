dab853c83a3e3186b2d0332472814b9096967250
arraen <arraen@arraen.org.ua>
2024-06-11 23:09:36 +0200

    configuration.nix - refacotring, add kb layouts

--- systems/roadray/configuration.nix
+++ systems/roadray/configuration.nix
@@ -10,6 +10,54 @@
       ./hardware-configuration.nix
     ];
 
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    patchelf
+    unzip
+    vim
+    wget
+    zsh
+    nix-ld
+    nix-index
+  ];
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Install neovim
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    iosevka
+    liberation_ttf
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    source-code-pro
+    uiua386
+    (nerdfonts.override { fonts = [ "SourceCodePro" ]; })
+  ];
+
+  # Use Flatpak, just in case
+  services.flatpak.enable = true;
+
+  # Define arraen user.
+  users.users.arraen = {
+    isNormalUser = true;
+    description = "arraen";
+    extraGroups = [ "networkmanager" "wheel" "audio" "input" "systemd-journal" "docker" ];
+    createHome = true;
+  };
+  
+  # Enable the PCSC service for yubikey authentication
+  services.pcscd.enable = true;
+
   # Nix configuration
   nix = {
     settings = {
@@ -29,7 +77,11 @@
   };
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 5;
+
+  };
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 
@@ -63,7 +115,7 @@
 
   # Configure keymap in X11
   services.xserver = {
-    xkb.layout = "us";
+    xkb.layout = "us,ua,se";
     xkb.variant = "";
   };
 
@@ -73,6 +125,8 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+
+  ## Audio config
   # Enable sound with pipewire.
   hardware.pulseaudio = {
     enable = false;
@@ -81,63 +135,15 @@
       load-module module-switch-on-connect
     ";
   };
+  # Enable realtime kit, needed for pulseaudio
   security.rtkit.enable = true;
+  # Enable pipewire service
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.arraen = {
-    isNormalUser = true;
-    description = "arraen";
-    extraGroups = [ "networkmanager" "wheel" "audio" "input" "systemd-journal" "docker" ];
-    createHome = true;
-  };
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    git
-    patchelf
-    unzip
-    vim
-    wget
-    zsh
-    nix-ld
-    nix-index
-  ];
-
-  # Fonts
-  fonts.packages = with pkgs; [
-    iosevka
-    liberation_ttf
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    source-code-pro
-    uiua386
-    (nerdfonts.override { fonts = [ "SourceCodePro" ]; })
-  ];
-
-  services.pcscd.enable = true;
-
-  # Use Flatpak, just in case
-  services.flatpak.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -147,23 +153,6 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.05";
 
 }
