29bf87eb31c637a36fbd9790f773e11c665e0ab9
arraen <arraen@arraen.org.ua>
2024-07-27 13:29:44 +0200

    Add KDE

--- flake.nix
+++ flake.nix
@@ -8,13 +8,20 @@
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Plasma
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, ... }: 
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, plasma-manager, ... }: 
   let
     system = "x86_64-linux";
-    owner = "arraen";
-    workLaptop = "roadray";
+    username = "arraen";
+    ownLaptop = "roadray";
     winWSL = "winray";
     overlay-unstable = final: prev: {
       unstable = import nixpkgs-unstable {
@@ -33,33 +40,40 @@
     }; 
   in {
     nixosConfigurations = {
-      ${workLaptop} = nixpkgs.lib.nixosSystem {
+      ${ownLaptop} = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = {
           inherit pkgs; 
         };
         modules = [
-          ./systems/${workLaptop}/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
+          ./systems/${ownLaptop}/configuration.nix
+          home-manager.nixosModules.home-manager{
             home-manager.extraSpecialArgs = { inherit pkgs; };
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.${owner} = import ./home/${workLaptop}/home.nix;
+            home-manager.users.${username} = import ./home/${username}/home.nix;
+            home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
           }
         ];
       };
     };
 
-    homeConfigurations.${owner} = home-manager.lib.homeManagerConfiguration {
+    homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
       inherit system;
       SpecialArgs = { inherit pkgs; };
       modules = [
-        ./home/${winWSL}/home.nix
+        inputs.plasma-manager.homeManagerModules.plasma-manager
+        ./home/${username}/home.nix
+        {
+          home = {
+            inherit username;
+            homeDirectory = "/home/${username}";
+          };
+        }
       ];
     };
 
-    packages.${system}.${owner} = self.homeConfigurations.${owner}.activationPackage;
+    packages.${system}.${username} = self.homeConfigurations.${username}.activationPackage;
   };
 }
 
